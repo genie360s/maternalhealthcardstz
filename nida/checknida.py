b55d049d8eb9f2b6a18eb55f30c9472fe5af045b
genie360s <alexgmkwizu@gmail.com>
2023-09-24 00:53:23 +0300

    add CONTRIBUTING.md

--- nida/checknida.py
+++ nida/checknida.py
@@ -1,3 +1,3 @@
 from nida import load_user
-user_detail = load_user(national_id='20010329151090000115', json = True)
+user_detail = load_user(national_id='20000407611010000228', json = True)
 print(user_detail)
